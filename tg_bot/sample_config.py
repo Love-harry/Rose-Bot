a169d0f650287a95b9a0d45dc4cebd5114839f8e
Harry Belema <84248272+Love-harry@users.noreply.github.com>
2022-04-22 16:49:16 +0100

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "5314993782:AAH_hbj6DFF0SFDLE-Ka2tuJL-Xe8htXQKc"
+    OWNER_ID = "1707757937"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "@plencoin_official"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
